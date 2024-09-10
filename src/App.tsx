374ead5e3e05f058f0642e502c4b755c2d5a6337
Sean Gay <sean.gay@gmail.com>
2024-09-10 17:58:28 +1000

    initial form handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import { useState } from "react";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
-  // const [alertVisible, setAlertVisibility] = useState(false);
-
-  // const groups = ["one", "two", "three"];
-
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        illo totam sit optio tenetur delectus assumenda possimus eveniet earum
-        ducimus commodi aliquid nobis maxime nemo vitae, cum impedit illum enim?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        illo totam sit optio tenetur delectus assumenda possimus eveniet earum
-        ducimus commodi aliquid nobis maxime nemo vitae, cum impedit illum enim?
-      </ExpandableText>
+      <Form></Form>
     </div>
   );
 }
