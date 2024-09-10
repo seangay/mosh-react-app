79f18593f7349af4d5486f2bf7d01d53ce7231bc
Sean Gay <sean.gay@gmail.com>
2024-09-10 18:10:25 +1000

    update person object on form submission

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,15 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
 
 const Form = () => {
+  const namedRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("asdasdas");
+    if (namedRef.current !== null) person.name = namedRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    console.log(person);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -11,13 +17,13 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={namedRef} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
