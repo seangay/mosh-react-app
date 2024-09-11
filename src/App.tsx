a0139a5ddd39de9c99f3e05b21904a6c07513600
Sean Gay <sean.gay@gmail.com>
2024-09-11 17:14:08 +1000

    added examples of effect dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import ExpenseTracker from "./expense-tracker/components/ExpenseTracker";
+import { useState } from "react";
+import ProductList from "./components/ProductList";
 
 function App() {
-  return <ExpenseTracker />;
+  const [category, setCategory] = useState("");
+
+  return (
+    <div>
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
+    </div>
+  );
 }
 
 export default App;
